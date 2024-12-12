a726881a23923a9442ed6386154606ad192233bc
Dev-Adalid <aleadalid447@gmail.com>
2024-12-12 15:58:08 -0300

    first commit

---
+++ Common/OE01/DTODSGeneral.cs
@@ -0,0 +1,28 @@
+﻿
+namespace BlazorOE01.Common.OE01
+{
+
+    public class Rootobject
+    {
+        public Dsgeneral dsGeneral { get; set; }
+    }
+
+    public class Dsgeneral
+    {
+        public Ttinvoice[] ttinvoice { get; set; }
+    }
+
+    public class Ttinvoice
+    {
+        public int Invoicenum { get; set; }
+
+        public int CustNum { get; set; }
+        public float Amount { get; set; }
+        public float TotalPaid { get; set; }
+        public float Adjustment { get; set; }
+        public float ShipCharge { get; set; }
+    }
+
+
+}
+
